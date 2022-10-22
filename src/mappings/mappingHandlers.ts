318d3d07a00c3af6ee6b143bd775dda02a292676
Pharaon1993 <pharaon1993@protonmail.com>
2022-10-22 00:53:57 +0000

    Added contract executions and loop airdrop.

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,4 +1,4 @@
-import { IbcEvent, ContractMessage } from "../types";
+import { IbcEvent, ContractMessage, LoopAirDropEvent } from "../types";
 import { CosmosEvent, CosmosMessage } from "@subql/types-cosmos";
 
 const EVENT_TYPES = {
@@ -65,3 +65,44 @@ export async function handleMsgExecuteContract(
   });
   await messageRecord.save();
 }
+
+export async function handleLoopAirDropEvent(
+  event: CosmosEvent
+): Promise<void> {
+  logger.info("Handling loop air drop event");
+
+  let amountObj = event.event.attributes.find(attr => attr.key === 'amount')
+  let senderObj = event.event.attributes.find(attr => attr.key === 'to')
+
+  let amount = '';
+  let sender = '';
+
+  if (amountObj && amountObj.value !== undefined && amountObj.value !== null) {
+    amount = amountObj.value;
+  }
+
+  /*
+  if (senderObj === undefined || senderObj === null || senderObj.value === undefined || senderObj.value === null) {
+    senderObj = event.event.attributes.find(attr => attr.key === 'sender')
+  }
+ */
+
+  if (senderObj && senderObj.value !== undefined && senderObj.value !== null) {
+    sender = senderObj.value
+  }
+
+
+  const loopAirDropEventRecord = LoopAirDropEvent.create({
+    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
+    blockHeight: BigInt(event.block.block.header.height),
+    txHash: event.tx.hash,
+    amount: BigInt(amount),
+    sender: sender,
+    eventDatetime: new Date(event.block.block.header.time),
+  });
+
+  logger.info("Saving loop air drop event")
+
+  await loopAirDropEventRecord.save();
+}
+
