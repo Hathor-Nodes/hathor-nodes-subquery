2af33245c5bf6f4bb9cccf3e92d226ad7ac633bd
Hathor <pharaon1993@protonmail.com>
2022-10-31 22:24:59 -0400

    Removed LoopAirdrop table.

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,43 @@
-import { Transaction, IbcEvent, ContractMessage, LoopAirDropEvent, LoopAirDropMessage } from "../types";
+import { Transaction, IbcEvent, ContractMessage } from "../types";
 import { CosmosEvent, CosmosMessage, CosmosTransaction} from "@subql/types-cosmos";
+
+export async function handleTransactionData(
+  tx: CosmosTransaction
+): Promise<void> {
+  const record = new Transaction(tx.hash);
+  record.blockHeight = BigInt(tx.block.block.header.height);
+  record.timestamp = new Date(tx.block.block.header.time);
+  record.payer = tx.decodedTx.authInfo.fee.payer;
+  record.granter = tx.decodedTx.authInfo.fee.granter;
+
+  if (tx.decodedTx.authInfo.fee.amount[0] === undefined) {
+    record.feeDenom = '';
+    record.feeAmount = BigInt(0);
+  } else {
+    record.feeDenom = tx.decodedTx.authInfo.fee.amount[0].denom;
+    record.feeAmount = BigInt(tx.decodedTx.authInfo.fee.amount[0].amount);    
+  } 
+
+  await record.save();
+}
+
+export async function handleMsgExecuteContract(
+  msg: CosmosMessage
+): Promise<void> {
+
+  const messageRecord = ContractMessage.create({
+    id: `${msg.tx.hash}-${msg.idx}`,    
+    blockHeight: BigInt(msg.block.block.header.height),
+    timestamp: new Date(msg.block.block.header.time),    
+    txHash: msg.tx.hash,
+    txFeeDenom: msg.tx.decodedTx.authInfo.fee.amount[0].denom,
+    weightedTxFeeAmount: BigInt(msg.tx.decodedTx.authInfo.fee.amount[0].amount) 
+      / BigInt(msg.tx.decodedTx.body.messages.length),
+    sender: msg.msg.decodedMsg.sender,
+    contract: msg.msg.decodedMsg.contract,
+  });
+  await messageRecord.save();
+}
 
 const EVENT_TYPES = {
   RECEIVE: 1,
@@ -14,7 +52,7 @@ const makeIbcEventRecord = (
     event.event.attributes.find((attr) => attr.key === "packet_data").value
   );
 
-  let amount = -1;
+  let amount = 0;
   if (packetData.amount !== undefined && packetData.amount !== null) {
       amount = packetData.amount;
   }
@@ -27,43 +65,17 @@ const makeIbcEventRecord = (
   return IbcEvent.create({
     id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
     blockHeight: BigInt(event.block.block.header.height),
+    timestamp: new Date(event.block.block.header.time),
+    eventType,    
     txHash: event.tx.hash,
     amount: BigInt(amount),
     denom: denom.split("/").at(-1),
-    eventDatetime: new Date(event.block.block.header.time),
-    eventType,
   });
 };
 
-export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
-  const record = new TransactionEntity(tx.tx.txhash);
-  record.blockHeight = BigInt(tx.block.block.block.header.height);
-  record.timestamp = tx.block.block.header.time;
-  record.txType = tx.tx.type;
-  record.txFeeDenom = tx.tx.auth_info.fee.amount.denom;
-  record.txFeeAmount = tx.tx.auth_info.fee.amount.amount;
-  await record.save();
-}
-
-export async function handleTransaction(
-  tx: CosmosTransaction
-): Promise<void> {
-     
-  const transactionEntity = Transaction.create({
-    id: `${tx.hash}`,
-    blockHeight: BigInt(tx.block.block.header.height),
-    txDate: new Date(tx.block.block.header.time),
-    sender: sender,
-    txFeeDenom: tx.auth_info.fee.amount.denom,
-    txFeeAmount: tx.auth_info.fee.amount.amount,
-  });
-
-  await transactionEntity.save();
-}
 export async function handleReceivePacketEvent(
   event: CosmosEvent
 ): Promise<void> {
-  logger.info("Handling receive packet event");
   const ibcEventRecord = makeIbcEventRecord(event, EVENT_TYPES.RECEIVE);
   await ibcEventRecord.save();
 }
@@ -71,38 +83,6 @@ export async function handleReceivePacketEvent(
 export async function handleTransferPacketEvent(
   event: CosmosEvent
 ): Promise<void> {
-  logger.info("Handling transfer packet event");
   const ibcEventRecord = makeIbcEventRecord(event, EVENT_TYPES.TRANSFER);
   await ibcEventRecord.save();
 }
-
-export async function handleMsgExecuteContract(
-  msg: CosmosMessage
-): Promise<void> {
-  logger.info("Handling message execute contract");
-  const messageRecord = ContractMessage.create({
-    id: `${msg.tx.hash}-${msg.idx}`,
-    blockHeight: BigInt(msg.block.block.header.height),
-    txHash: msg.tx.hash,
-    sender: msg.msg.decodedMsg.sender,
-    contract: msg.msg.decodedMsg.contract,
-    messageDatetime: new Date(msg.block.block.header.time),
-  });
-  await messageRecord.save();
-}
-
-export async function handleLoopAirDropMessage(
-    msg: CosmosMessage
-): Promise<void> {
-
-    const airdropClaim = LoopAirDropMessage.create({
-        id: `${msg.tx.hash}-${msg.idx}`,
-        blockHeight: BigInt(msg.block.block.header.height),
-        sender: msg.msg.decodedMsg.sender || '',
-        amount: msg.msg.decodedMsg.msg.claim.amount || 0,
-    });
-
-    logger.info("Saving loop air drop message.")
-    await airdropClaim.save();
-}
-
